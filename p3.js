268494bd15c87b25a69c1e7a5bea0f2cd84dc310
NerellaDhakshitha <141156739+NerellaDhakshitha@users.noreply.github.com>
2024-05-23 12:05:57 +0530

    Add Conditional Statements

---
+++ p3.js
@@ -0,0 +1,19 @@
+let marks=prompt("Enter marks:");
+if(marks>=90 && marks<=100){
+    console.log("Grade=A");
+}
+else if(marks>=70 && marks<=89) {
+    console.log("Grade=B");
+}
+else if(marks>=60 && marks<=69) {
+    console.log("Grade=C");
+}
+else if(marks>=50 && marks<=59) {
+    console.log("Grade=D");
+}
+else if(marks>=40 && marks<=49) {
+    console.log("Grade=E");
+}
+else{
+    console.log("Grade=F");
+}
