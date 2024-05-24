8afb7cbe121edb5f30e7d2f7fe55fba395c0c7d5
NerellaDhakshitha <141156739+NerellaDhakshitha@users.noreply.github.com>
2024-05-24 16:27:51 +0530

    Add whileloop

---
+++ p4.js
@@ -0,0 +1,6 @@
+let gameNum=5;
+let userNum=prompt("Guess the Number:");
+while(userNum!=gameNum){
+    userNum=prompt(" You entered the wrong number,Guess again:");
+}
+console.log("Congractulations!!!!");
