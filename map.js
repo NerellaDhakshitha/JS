aeff76f83795072324e4aa3cf5dfb47743830390
NerellaDhakshitha <141156739+NerellaDhakshitha@users.noreply.github.com>
2024-05-25 12:53:15 +0530

    map method

---
+++ map.js
@@ -0,0 +1,7 @@
+let num=[1,2,3,4,5];
+const newArr=num.map((val)=>
+{                                           //map is used to create a new array from the return values of a array
+    return val;
+})
+
+console.log(newArr);
