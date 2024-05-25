ca5d72e3218cb88b7ad382f0eb845a0bd25fef5a
NerellaDhakshitha <141156739+NerellaDhakshitha@users.noreply.github.com>
2024-05-25 12:52:31 +0530

    forEach

---
+++ foreach.js
@@ -0,0 +1,11 @@
+let arr=[1,2,3,4,5];
+const square=(num)=>{
+    console.log(num*num);    //for each is used to do some calculations of each element
+};
+arr.forEach(square);
+
+
+
+/*arr.forEach((val)=>{     //we can write like this too
+    console.log(val);
+})*/
